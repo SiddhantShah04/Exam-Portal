cf427be8952f4d4933ccdd1ec3877507620757c7
sidd044 <siddhantshah04@gmail.com>
2019-11-14 06:58:00 +0000

    k

---
+++ create.py
@@ -0,0 +1,12 @@
+import os
+
+from flask import Flask, render_template, request
+from models import *
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
+
+def main():
+    db.create_all()
