4d799d15401b9f67288f6e3011a5a3f4553ab591
sidd044 <42845723+sidd044@users.noreply.github.com>
2020-02-07 12:49:21 +0530

    Update application.py

--- application.py
+++ application.py
@@ -11,7 +11,7 @@ from models import *
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://lxwhjuhnkqxjzl:4b56351c072ef8f11a3b0ecde34fccf51b7e48d70986d4ecbdbf77ddc243cf7b@ec2-174-129-33-97.compute-1.amazonaws.com:5432/dik6pjhuaf1ne"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 app.secret_key = "f*"
